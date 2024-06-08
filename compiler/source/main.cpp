678f4c68b1cf7c1d7466c687481c235d4206c087
adriensalon <adrien.salon@live.fr>
2024-06-08 06:29:20 +0200

    waypoint

--- compiler/source/main.cpp
+++ compiler/source/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <functional>
 #include <unordered_map>
 #include <string>
 #include <vector>
@@ -6,15 +7,13 @@
 #include <core/shader.hpp>
 #include <core/texture.hpp>
 
-namespace lucaria {
-
-/// @brief 
+/// @brief Represents a map containing every argument for each command
 using commands_map = std::unordered_map<std::string, std::vector<std::string>>;
 
-/// @brief 
-/// @param argc 
-/// @param argv 
-/// @return 
+/// @brief Extracts all commands with arguments from main parameters
+/// @param argc the first parameter taken by the main function
+/// @param argv the second parameter taken by the main function
+/// @return the map containings all the commands
 commands_map extract_args(int argc, char* argv[])
 {
     commands_map _commands;
@@ -67,7 +66,7 @@ std::filesystem::path process_input_command(const commands_map& commands)
         std::cerr << "The path provided with option -i must be an existing directory" << std::endl;
         std::terminate();
     }
-    std::cout << "-- Assets input directory at " << _input_dir << std::endl;
+    // std::cout << "-- Assets input directory at " << _input_dir << std::endl;
     return _input_dir;
 }
 
@@ -89,56 +88,91 @@ std::filesystem::path process_output_command(const commands_map& commands)
         std::cerr << "The path provided with option -o must be an existing directory" << std::endl;
         std::terminate();
     }
-    std::cout << "-- Assets output directory at " << _output_dir << std::endl;
+    // std::cout << "-- Assets output directory at " << _output_dir << std::endl;
     return _output_dir;
+}
+
+/// @brief 
+/// @param base_dir 
+/// @param callback 
+void iterate_recursive(const std::filesystem::path base_dir, const std::function<void(const std::filesystem::path&)> callback)
+{
+    try {
+        for (const std::filesystem::directory_entry& _entry : std::filesystem::recursive_directory_iterator(base_dir)) {
+            if (std::filesystem::is_regular_file(_entry.status())) {
+                callback(_entry.path());
+            }
+        }
+    } catch (const std::filesystem::filesystem_error& e) {
+        std::cerr << "Filesystem error " << e.what() << std::endl;
+        std::terminate();
+    } catch (const std::exception& e) {
+        std::cerr << "General exception " << e.what() << std::endl;
+        std::terminate();
+    }
+}
+
+/// @brief 
+/// @param base_dir 
+/// @param file_path 
+/// @return 
+std::filesystem::path substract_relative(const std::filesystem::path base_dir, const std::filesystem::path file_path)
+{
+    try {
+        if (std::filesystem::exists(base_dir) && std::filesystem::exists(file_path) && std::filesystem::is_directory(base_dir)) {
+            if (file_path.string().find(base_dir.string()) == 0) {
+                return std::filesystem::relative(file_path, base_dir);
+            } else {
+                std::cerr << "The target path is not within the base directory" << std::endl;
+                std::terminate();
+            }
+        } else {
+            std::cerr << "Base directory or target path does not exist" << std::endl;
+            std::terminate();
+        }
+    } catch (const std::filesystem::filesystem_error& e) {
+        std::cerr << "Filesystem error " << e.what() << std::endl;
+        std::terminate();
+    } catch (const std::exception& e) {
+        std::cerr << "General exception " << e.what() << std::endl;
+        std::terminate();
+    }
 }
 
 /// @brief 
 /// @param input_path 
 /// @param output_path 
-void compile_resource(const std::filesystem::path& input_path, const std::filesystem::path& output_path)
+void compile_resource(const std::filesystem::path& input_file, const std::filesystem::path& output_file)
 {
-    // // get output file name and path
-    // std::string _name;
-    // _name = json_get_string(json_root, "name", []() {
-    //     console::log("error : ", console::text_color::red);
-    //     exceptions::throw_error();
-    // });
-    // std::filesystem::path _output_file_path = output_path / (_name + ".bin");
-    // file::create_directory_if_needed(_output_file_path.parent_path());
-
-    // // get resource type and dispatch
-    // std::string _type;
-    // _type = json_get_string(json_root, "type", []() {
-    //     console::log("error : ", console::text_color::red);
-    //     exceptions::throw_error();
-    // });
-    // if (_type == "sound")
-    //     audio::compile_sound(json_root, _output_file_path);
-    // else if (_type == "mesh")
-    //     graphics::compile_mesh(json_root, _output_file_path);
-    // else if (_type == "texture")
-    //     graphics::compile_texture(json_root, _output_file_path);
-    // else if (_type == "cubemap")
-    //     graphics::compile_cubemap(json_root, _output_file_path);
-    // else if (_type == "shader")
-    //     graphics::compile_shader(json_root, _output_file_path);
-    // else {
-    //     console::log("error : invalid resource type '" + _type + "' is not 'audio' | 'mesh' | 'texture' | 'cubemap' | 'shader'", console::text_color::red);
-    //     exceptions::throw_error();
-    // }
-
-    // std::filesystem::path _resolved_output_file_path = std::filesystem::current_path() / _output_file_path;
-    // console::log("message : asset type '" + _type + "' compiled at '" + _resolved_output_file_path.generic_string() + "'", console::text_color::green);
-}
-
+    const std::string _extension = input_file.extension().generic_string();
+    if (_extension == ".ttf") {
+        // lucaria::compile_font(lucaria::import_font(input_file), output_file);
+    } else if (_extension == ".obj") {
+        lucaria::compile_mesh(lucaria::import_mesh(input_file), output_file);
+    } else if (_extension == ".glsl") {
+        lucaria::compile_shader(lucaria::import_shader(input_file), output_file);    
+    } else if (_extension == ".wav") {
+        // lucaria::compile_sound(lucaria::import_sound(input_file), output_file); 
+    } else if (_extension == ".jpg") {
+        lucaria::compile_texture(lucaria::import_texture(input_file), output_file); 
+    } else {
+        std::cerr << "Invalid input file with extension " << _extension << std::endl;
+        std::terminate();
+    }
 }
 
 int main(int argc, char* argv[])
 {
-    lucaria::commands_map _commands = lucaria::extract_args(argc, argv);
-    if (lucaria::process_help_command(_commands)) return 0;
-    std::filesystem::path _input_dir = lucaria::process_input_command(_commands);
-    std::filesystem::path _output_dir = lucaria::process_output_command(_commands);
+    commands_map _commands = extract_args(argc, argv);
+    if (process_help_command(_commands)) return 0;
+    std::filesystem::path _input_dir = process_input_command(_commands);
+    std::filesystem::path _output_dir = process_output_command(_commands);
+    iterate_recursive(_input_dir, [&] (const std::filesystem::path& _input_file) {
+        std::filesystem::path _relative_input_file = substract_relative(_input_dir, _input_file);
+        std::filesystem::path _relative_output_file = _relative_input_file;
+        _relative_output_file.replace_extension(".bin");
+        std::filesystem::path _output_file = _output_dir / _relative_output_file;
+        compile_resource(_input_file, _output_file);
+    });
     return 0;
 }
