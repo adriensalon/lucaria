1ae6895a4004c5e71c39bc7c28b5d8e99d0fcf76
adriensalon <adrien.salon@live.fr>
2024-07-18 03:51:05 +0200

    waypoint

--- client/source/game/levels/blockout/test.cpp
+++ client/source/game/levels/blockout/test.cpp
@@ -11,6 +11,6 @@ void level_blockout_test(entt::registry& registry)
     registry.emplace<model_component<model_shader::blockout>>(_test_entity)
         .mesh(fetch_mesh("assets/blockout_test.bin"));
 
-    // registry.emplace<collider_component<collider_algorithm::ground>>(_test_entity)
-    //     .navmesh(fetch_navmesh("assets/blockout_test.bin"));
+    registry.emplace<collider_component<collider_algorithm::ground>>(_test_entity)
+        .navmesh(fetch_navmesh("assets/blockout_test_ground.bin"));
 }
