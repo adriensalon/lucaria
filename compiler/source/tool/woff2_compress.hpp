b8f7695c3abeb3ccc40cb3c66000a551c90c5017
adriensalon <adrien.salon@live.fr>
2024-07-25 02:24:12 +0200

    Revert "waypoint"
    This reverts commit 8b2b66e4e3d2ffe27547543f2b0f7e2b273d8a50.

--- compiler/source/tool/woff2_compress.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <filesystem>
-
-void execute_woff2_compress(const std::filesystem::path& woff2_compress_exe, const std::filesystem::path& input_path, const std::filesystem::path& output_path);
