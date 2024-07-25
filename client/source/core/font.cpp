dbc4e2ae385dc11768cc25943cb25499a9ae1a22
adriensalon <adrien.salon@live.fr>
2024-07-25 02:14:18 +0200

    Revert "waypoint"
    This reverts commit fee051f2a3aba7125efdc7cbc78ae408acc43025.

--- client/source/core/font.cpp
+++ client/source/core/font.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
 
-#include <backends/imgui_impl_opengl3.h>
 #include <woff2/decode.h>
 
-#include <core/fetch.hpp>
 #include <core/font.hpp>
 
 namespace detail {
@@ -15,28 +13,7 @@ static std::unordered_map<std::string, std::promise<std::shared_ptr<font_ref>>> 
 std::shared_future<std::shared_ptr<font_ref>> fetch_font(const std::filesystem::path& font_path)
 {
     std::promise<std::shared_ptr<font_ref>>& _promise = detail::promises[font_path.string()];
-    fetch_file(font_path, [&_promise](const std::vector<char>& font_bytes) {
-        const std::uint8_t* _raw_ptr = reinterpret_cast<const uint8_t*>(font_bytes.data());
-        std::string _output_str(std::min(woff2::ComputeWOFF2FinalSize(_raw_ptr, font_bytes.size()), woff2::kDefaultMaxSize), 0);
-        woff2::WOFF2StringOut _woff2out(&_output_str);
-        if (!woff2::ConvertWOFF2ToTTF(_raw_ptr, font_bytes.size(), &_woff2out)) {
-#if LUCARIA_DEBUG
-            std::cout << "Impossible to decode woff2 font." << std::endl;
-            std::terminate();
-#endif
-        }
-        _promise.set_value(std::shared_ptr<ImFont>(ImGui::GetIO().Fonts->AddFontFromMemoryTTF(_output_str.data(), _output_str.size(), 16.f)));
-        ImGui::GetIO().Fonts->AddFontDefault();
-        if (!ImGui::GetIO().Fonts->Build()) {
-#if LUCARIA_DEBUG
-            std::cout << "Impossible build ImGui font." << std::endl;
-            std::terminate();
-#endif
-        } else {
-            ImGui_ImplOpenGL3_DestroyFontsTexture();
-            ImGui_ImplOpenGL3_CreateFontsTexture();
-        }
-    });
+
 
     return _promise.get_future();
 }
--- client/source/core/font.hpp
+++ client/source/core/font.hpp
@@ -6,6 +6,6 @@
 
 #include <imgui.h>
 
-using font_ref = ImFont;
+using font_ref = std::unique_ptr<ImFont>;
 
 std::shared_future<std::shared_ptr<font_ref>> fetch_font(const std::filesystem::path& font_path);
